560c5dd23cc4eef695b880ccb82bad95e99ffd35
Akayy <techakayy@gmail.com>
2023-09-25 11:56:42 +1000

    Meta updates

--- src/site.ts
+++ src/site.ts
@@ -1,9 +1,6 @@
-const image = new URL('./screenshots/image.png', import.meta.url).href
-
 export default {
   title: `Vue Designer`,
   description: 'Vue Designer îles Tailwind CSS - Quick start template',
-  image,
   author: 'Pinegrow',
   url: 'https://pg-iles-tailwindcss.netlify.app',
   nav: [
